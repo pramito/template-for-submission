99b75d82f4fc2ae50c478048da9361630c3549ad
pramito <130637070+pramito@users.noreply.github.com>
2023-04-14 14:09:26 +0600

    Update q1.sql

--- transform/models/mysubmission/q1.sql
+++ transform/models/mysubmission/q1.sql
@@ -1,11 +1,7 @@
-{{
-    config(
-        materialized='view'
-    )
-}}
-
-WITH customers AS {
-    SELECT
-      customer_id,
-      ...
-}
+SELECT
+  country_name,
+  ROUND(SUM(gmv_local),2) AS gmv_total
+FROM
+  `foodpanda-383505.orders.orders`
+GROUP BY
+  country_name
